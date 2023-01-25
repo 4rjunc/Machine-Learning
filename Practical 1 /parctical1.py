b4d5b1a7f414b426252daac66ccf548ee7f7d685
H4K3R13 <mit2224366@gmail.com>
2023-01-25 12:29:28 +0530

    Adding jupyter notebook

---
+++ Practical 1 /parctical1.py
@@ -0,0 +1,23 @@
+#%matplotlib inline
+
+#import required library 
+import random
+import datetime
+import numpy as np
+import pandas as pd
+from random import randrange 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style('whitegrid')
+sns.set_context('talk')
+
+params = {'legend.fontsize':'small',
+          'figure.figsize':(10,5),
+          'axes.labelsize':'medium',
+          'axes.titlesize':'medium',
+          'xtick.labelsize':'medium',
+          'ytick.labelsize':'medium'}
+
+plt.rcParams.update(params)
+
