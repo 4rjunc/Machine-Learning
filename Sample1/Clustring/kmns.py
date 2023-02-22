ed177f279ab4527d7ed4a080851f76f1819082f6
H4K3R13 <mit2224366@gmail.com>
2023-02-22 14:26:36 +0530

    Clustering Kmedoids

--- Sample1/Clustring/kmns.py
+++ Sample1/Clustring/kmns.py
@@ -12,8 +12,6 @@ from sklearn import preprocessing
 df = pd.read_csv("./data.csv")
 df.head(100)
 
-
-
 df = df.drop('id',axis=1)
 df = df.drop('Unnamed: 32',axis=1)
 #Mapping Benign to 0 and Malignant to 1
@@ -31,19 +29,22 @@ from sklearn.manifold import TSNE
 tsne = TSNE(verbose=1, perplexity=40, n_iter=4000)
 Y = tsne.fit_transform(X)
 
-
 from sklearn.cluster import KMeans
 kmns = KMeans(n_clusters=2, init='k-means++',n_init=50,max_iter=300,tol=0.0001)
 kY = kmns.fit_predict(X)
-
 f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-
 ax1.scatter(Y[:,0],Y[:,1], c = kY, cmap = "jet",edgecolor="None", alpha=0.35)
 ax1.set_title('k-means clustering plot')
-
 ax2.scatter(Y[:,0],Y[:,1], c = datas['diagnosis'], cmap = "jet",edgecolor="None", alpha=0.35)
 ax2.set_title('Actual cluster')
 
+from sklearn_extra.cluster import KMedoids
+kmedoids = KMedoids(n_clusters=2, random_state=0)
+kY = kmedoids.fit_predict(X)
+ax1.scatter(Y[:,0],Y[:,1], c = kY, cmap = "jet",edgecolor="None", alpha=0.35)
+ax1.set_title('k-medoids clustering plot')
+ax2.scatter(Y[:,0],Y[:,1], c = datas['diagnosis'], cmap = "jet",edgecolor="None", alpha=0.35)
+ax2.set_title('Actual cluster')
 
 
 
