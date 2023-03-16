fa2701732dc78ef98bd4d69f94733bbe997f502f
H4K3R13 <mit2224366@gmail.com>
2023-03-16 10:13:01 +0530

    PCA@

--- PrincipleComponetAnalysis/pca2.py
+++ PrincipleComponetAnalysis/pca2.py
@@ -10,3 +10,17 @@ import numpy as np
 
 mydata = mypd.read_csv("./Iris_data.csv")
 print(mydata)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaled_data = mypd.DataFrame(scaler.fit_transform(mydata))
+print(scaled_data)
+mysb.heatmap(scaled_data.corr())
+
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 2)
+pca.fit(mydata)
+data_pca = pca.transform(mydata)
+data_pca = mypd.DataFrame(data_pca,columns=['PC1','PC2'])
+print(data_pca)
